d210241c9988dd647fe24c34a8dd74b926748c8f
Chad Liu <liu_q@worksap.co.jp>
2016-08-02 13:06:22 +0800

    Remove status

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ app.get('/', function(request, response) {
 app.post('/slack', function(request, response) {
     console.log(request.body);
     response.json({"text": "received"})
-    response.sendStatus(200);
 });
 
 
