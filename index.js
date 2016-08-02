fd96b4da153c0b39043120f35ecb800626add19c
Chad Liu <liu_q@worksap.co.jp>
2016-08-02 12:43:13 +0800

    Remove getting-started origin files

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
 var app = express();
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,11 +14,17 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
+
+
+app.post('/slack', function(request, response) {
+    console.log(request.body);
+    response.sendStatus(200);
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
