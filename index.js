087067d7f4064cc12a8ca60e6f8d4000db7cc58a
Chad Liu <liu_q@worksap.co.jp>
2016-08-02 14:05:10 +0800

    only print out the thing I interested

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ var upload = multer(); // for parsing multipart/form-data
 
 var app = express();
 
+var config = {
+    token: process.env.TOKEN ||'', // get from https://api.slack.com/web#basics
+    income_url: process.env.INCOME_URL || '',
+    outcome_token: process.env.OUTCOME_TOKEN || '',
+    // optionals
+    floodProtection: true,
+    silent: false // keep the bot quiet
+};
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -16,7 +25,21 @@ app.get('/', function(request, response) {
 
 
 app.post('/slack', function(request, response) {
-    console.log(request.body);
+    let playload = request.body;
+
+    if (config.token !== '' && config.token !== playload.token) {
+        response.sendStatus(404);
+        response.end();
+        return;
+    }
+
+    console.log('Text: ' + playload.text);
+    console.log('user_name: ' + playload.user_name);
+    console.log('channel_name: ' + playload.channel_name);
+    // console.log('channel_id: ' + playload.channel_id);
+    // console.log('user_id: ' + playload.user_id);
+    // console.log('team_id: ' + playload.team_id);
+    // console.log('token: ' + playload.token);
     response.sendStatus(200);
     response.end();
 });
